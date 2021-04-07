b197a219761ba4c8ecc1ef714c275de59093a270
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-04-07 21:45:49 +0300

    Update proiect

--- Echipe.cpp
+++ Echipe.cpp
@@ -10,7 +10,7 @@ FCA::FCA()
 	supercupa_romaniei = 0;
 }
 
-FCA::FCA(FCA& copie_arges)
+FCA::FCA(const FCA& copie_arges)
 {
 	this->nume = copie_arges.nume;
 	this->an_infiintare = copie_arges.an_infiintare;
@@ -28,6 +28,7 @@ FCA FCA::operator=(const FCA& copie_arges) {
 	return *this;
 }
 
+/*
 std::ostream& operator<<(std::ostream& output, const FCA& afisare_arges) {
 	output << "Echipa de fotbal: " << afisare_arges.nume << " a fost infiintata in anul: " << afisare_arges.an_infiintare << ".\n";
 	if (afisare_arges.campionat) {
@@ -50,38 +51,19 @@ std::ostream& operator<<(std::ostream& output, const FCA& afisare_arges) {
 	}
 	return output;
 }
+*/
 
-FCA::FCA(std::string nume, int an_infiintare, int campionat, int cupa_romaniei, int supercupa_romaniei) : Participanti(nume, an_infiintare, campionat, cupa_romaniei, supercupa_romaniei)
-{
+
+FCA::FCA(std::string nume, int an_infiintare, int campionat, int cupa_romaniei, int supercupa_romaniei){
 	this->nume = "FC Arges Pitesti";
 
 }
 
-void FCA::afisare() {
-	std::cout << "Echipa de fotbal: " << this->nume << " a fost infiintata in anul: " << this->an_infiintare << ".\n";
-	if (campionat) {
-		std::cout << "Echipa de fotbal: " << this->nume << " a castigat campionatul de: " << this->campionat << " ori." << "\n";
-	}
-	else {
-		std::cout << "Echipa de fotbal: " << this->nume << "nu a castigat pana in acest moment campionatul." << "\n";
-	}
-	if (cupa_romaniei) {
-		std::cout << "Echipa de fotbal: " << this->nume << "a castigat cupa Romaniei de: " << this->cupa_romaniei << " ori." << "\n";
-	}
-	else {
-		std::cout << "Echipa de fotbal: " << this->nume << " nu a castigat pana in acest moment cupa Romaniei." << "\n";
-	}
-	if (supercupa_romaniei) {
-		std::cout << "Echipa de fotbal: " << this->nume << " a castigat supercupa Romaniei de: " << this->supercupa_romaniei << " ori." << "\n";
-	}
-	else {
-		std::cout << "Echipa de fotbal: " << this->nume << " nu a castigat pana in acest moment supercupa Romaniei." << "\n";
-	}
-}
-
+/*
 void FCA::detalii() {
 	std::cout << "\n..."; // Aici or sa apara detalii suplimentare legate de istoria clubului: jucatori importanti, performante europene, performante in campionatul intern, etc.
 }
+*/
 
 FCA::~FCA() {
 	nume = "";
@@ -89,6 +71,13 @@ FCA::~FCA() {
 	campionat = 0;
 	cupa_romaniei = 0;
 	supercupa_romaniei = 0;
+}
+
+void FCA::coeficient() {
+	//Modalitatea in care voi alege coeficientul echipelor o sa se modifice pe parcurs, dupa ce adaug mai multe echipe
+	if (campionat >= 15) {
+		std::cout << "Echipa: " << this->nume << " ocupa locul" << int((this->campionat * 3) / 4);
+	}
 }
 
 
@@ -100,12 +89,13 @@ FCDB::FCDB() {
 	supercupa_romaniei = 0;
 }
 
-FCDB::FCDB(std::string nume, int an_infiintare, int campionat, int cupa_romaniei, int supercupa_romaniei) : Participanti(nume, an_infiintare, campionat, cupa_romaniei, supercupa_romaniei)
-{	
+
+FCDB::FCDB(std::string nume, int an_infiintare, int campionat, int cupa_romaniei, int supercupa_romaniei){
 	this->nume = "FC Dinamo Bucuresti";
 
 }
 
+/*
 void FCDB::afisare() {
 	std::cout << "Echipa de fotbal: " << this->nume << " a fost infiintata in anul: " << this->an_infiintare << ".\n";
 	if (campionat) {
@@ -127,10 +117,13 @@ void FCDB::afisare() {
 		std::cout << "Echipa de fotbal: " << this->nume << " nu a castigat pana in acest moment supercupa Romaniei." << "\n";
 	}
 }
+*/
 
+/*
 void FCDB::detalii() {
 	std::cout << "\n..."; // Aici or sa apara detalii suplimentare legate de istoria clubului: jucatori importanti, performante europene, performante in campionatul intern, etc.
 }
+*/
 
 FCDB::~FCDB() {
 	nume = "";
@@ -139,4 +132,11 @@ FCDB::~FCDB() {
 	cupa_romaniei = 0;
 	supercupa_romaniei = 0;
 
+}
+
+void FCDB::coeficient() {
+	//Modalitatea in care voi alege coeficientul echipelor o sa se modifice pe parcurs, dupa ce adaug mai multe echipe
+	if (campionat >= 11) {
+		std::cout << "Echipa: " << this->nume << " ocupa locul" << int((this->campionat * 9) / 5);
+	}
 }
