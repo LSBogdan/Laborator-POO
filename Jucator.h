34eb430418d47839986570051871a679d061d7fd
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-04-08 18:07:46 +0300

    Tema1 Update

--- Jucator.h
+++ Jucator.h
@@ -10,14 +10,17 @@ protected:
     int varsta, experienta;
     float salariu;
 public:
+   
     Jucator();
 
     Jucator(const std::string& post, const std::string& nume, const std::string& prenume, int varsta, int experienta,
         float salariu);
 
+   
     friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator);
     friend std::istream& operator>>(std::istream& is, Jucator& jucator);
 
+    
     const std::string& getPost() const;
 
     void setPost(const std::string& post);
@@ -42,6 +45,7 @@ public:
 
     void setSalariu(float salariu);
 
+    
     virtual void mariri_salariu() {
         if (experienta == 2)
             salariu *= 1.2;
@@ -54,7 +58,7 @@ public:
         std::cout << "Noul tau salariu este: " << this->salariu << " euro." << "\n\n";
     }
 
-
+   
     virtual ~Jucator();
 
 };
