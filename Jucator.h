b55db4716be8775d81f9aa6da8b634e6c8d71560
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-04-22 22:45:18 +0300

    Tema2

--- Jucator.h
+++ Jucator.h
@@ -1,11 +1,3 @@
-//
-// Created by Bogdan on 22/04/2021.
-//
-
-#ifndef TEMA_POO_JUCATOR_H
-#define TEMA_POO_JUCATOR_H
-
-
 #include <string>
 #include <ostream>
 #include <iostream>
@@ -16,16 +8,16 @@ protected:
     int varsta, experienta;
     float salariu;
 public:
-    //--CONSTRUCTORI
+    
     Jucator();
 
     Jucator(const std::string& nume, const std::string& prenume, int varsta, int experienta);
 
-    //--FUNCTII OPERATORI
+    
     friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator);
     friend std::istream& operator>>(std::istream& is, Jucator& jucator);
 
-    //-- GETTERI SI SETTERI
+   
     const std::string& getPost() const;
 
     void setPost(const std::string& post);
@@ -50,24 +42,8 @@ public:
 
     void setSalariu(float salariu);
 
-    //--FUNCTII VIRTUALE
     virtual void mariri_salariu() = 0;
-
-    /*virtual void mariri_salariu() {
-        if (experienta == 2)
-            salariu *= 1.2;
-        else if (experienta == 3)
-            salariu *= 1.3;
-        else if (experienta == 4)
-            salariu *= 1.4;
-        else if (experienta >= 5)
-            salariu *= 1.5;
-        std::cout << "Noul tau salariu este: " << this->salariu << " euro." << "\n";
-    }
-     */
-    //--DESTRUCTOR
+   
     virtual ~Jucator();
 
 };
-
-#endif //TEMA_POO_JUCATOR_H
