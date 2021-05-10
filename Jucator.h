9b3a4106874439784b53fdad79fd0c57013d8c1c
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-05-10 08:59:06 +0300

    Update Tema2

--- Jucator.h
+++ Jucator.h
@@ -1,49 +1,46 @@
-#include <string>
-#include <ostream>
 #include <iostream>
+#include <ostream>
+#include <string>
+
 
 class Jucator {
+
 protected:
+
     std::string post, nume, prenume;
     int varsta, experienta;
     float salariu;
+
 public:
-    
+
     Jucator();
-
     Jucator(const std::string& nume, const std::string& prenume, int varsta, int experienta);
+    virtual ~Jucator();
 
-    
     friend std::ostream& operator<<(std::ostream& os, const Jucator& jucator);
+
     friend std::istream& operator>>(std::istream& is, Jucator& jucator);
 
-   
     const std::string& getPost() const;
-
     void setPost(const std::string& post);
 
     const std::string& getNume() const;
-
     void setNume(const std::string& nume);
 
     const std::string& getPrenume() const;
-
     void setPrenume(const std::string& prenume);
 
     int getVarsta() const;
-
     void setVarsta(int varsta);
 
     int getExperienta() const;
-
     void setExperienta(int experienta);
 
     float getSalariu() const;
-
     void setSalariu(float salariu);
 
     virtual void mariri_salariu() = 0;
-   
-    virtual ~Jucator();
+
+    virtual void ver_exp() = 0;
 
 };
