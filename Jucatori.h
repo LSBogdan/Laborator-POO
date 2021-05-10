0ac53c3f8643053dc0ab88bea9c70120411999a0
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-05-10 09:01:41 +0300

    Update Tema2

--- Jucatori.h
+++ Jucatori.h
@@ -1,51 +1,57 @@
 #include "Jucator.h"
+#include <exception>
 
-class Portar:
-        public Jucator {
+class Portar: public Jucator {
+
 public:
+
     Portar();
-
     Portar(const std::string &nume, const std::string &prenume, int varsta, int experienta);
-
     virtual ~Portar();
 
     void mariri_salariu();
+
+    void ver_exp();
+
 };
 
-class Fundas:
-        public Jucator{
+class Fundas: public Jucator{
+
 public:
+
     Fundas();
-
     Fundas(const std::string &nume, const std::string &prenume, int varsta, int experienta);
-
     virtual ~Fundas();
 
     void mariri_salariu();
+
+    void ver_exp();
+
 };
 
-class Mijlocas:
-        public Jucator{
+class Mijlocas: public Jucator{
+
 public:
+
     Mijlocas();
-
     Mijlocas(const std::string &nume, const std::string &prenume, int varsta, int experienta);
-
     virtual ~Mijlocas();
 
     void mariri_salariu();
+
+    void ver_exp();
+
 };
 
-class Atacant:
-        public Jucator{
+class Atacant: public Jucator{
+
 public:
     Atacant();
-
     Atacant(const std::string &nume, const std::string &prenume, int varsta, int experienta);
-
     virtual ~Atacant();
 
     void mariri_salariu();
 
-};
+    void ver_exp();
 
+};
