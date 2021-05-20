772cddbc8db3e152a472314d59752c8cd6433caf
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-05-20 10:19:39 +0300

    Update Tema2

--- main.cpp
+++ main.cpp
@@ -13,64 +13,44 @@ int main() {
 
     std::cout << "Cativa jucatori din lotul echipei:" << "\n\n";
     Portar portar_ag("Greab", "Alexandru", 28, 7);
-    std::cout << portar_ag << std::endl;
+    std::cout << portar_ag << "\n";
     portar_ag.mariri_salariu();
-    try
-    {
-        portar_ag.ver_exp();
-    }
-    catch (const std::exception& e)
-    {
-        std::cout<<e.what();
-    }
     arges.add_jucator(portar_ag);
-
+    std::cout<< "\n";
 
     Fundas fundas_ag("Boldor", "Deian", 25, 8);
-    std::cout << fundas_ag << std::endl;
+    std::cout << fundas_ag << "\n\n";
     fundas_ag.mariri_salariu();
-    try
-    {
-        fundas_ag.ver_exp();
-    }
-    catch (const std::exception& e)
-    {
-        std::cout<<e.what();
-    }
     arges.add_jucator(fundas_ag);
-
+    std::cout<< "\n";
 
     Mijlocas  mijlocas_ag("Palic", "Antun", 32, 15);
-    std::cout << mijlocas_ag << std::endl;
+    std::cout << mijlocas_ag << "\n";
     mijlocas_ag.mariri_salariu();
-    try
-    {
-        mijlocas_ag.ver_exp();
-    }
-    catch (const std::exception& e)
-    {
-        std::cout<<e.what();
-    }
     arges.add_jucator(mijlocas_ag);
-
+    std::cout<< "\n";
 
     Atacant atacant_ag("Malele", "Cephas", 27, 9);
-    std::cout << atacant_ag << std:: endl;
+    std::cout << atacant_ag << "\n";
     atacant_ag.mariri_salariu();
+    arges.add_jucator(atacant_ag);
+    std::cout<<"\n";
+
     try
-    {
+    {   portar_ag.ver_exp();
+        fundas_ag.ver_exp();
+        mijlocas_ag.ver_exp();
         atacant_ag.ver_exp();
     }
     catch (const std::exception& e)
     {
         std::cout<<e.what();
     }
-    arges.add_jucator(atacant_ag);
-
     //arges.afisare_lot();
 
+
     ClubSportiv fcdb("FC Dinamo Bucuresti", 1948, 18, 13, 2, Detalii("FC Dinamo Bucuresti, pe scurt Dinamo, este un club de fotbal din Bucuresti, Romania, care evolueaza in Liga I. Fondata in 1948, echipa a debutat direct in prima divizie a Romaniei si nu a retrogradat niciodata. Din punct de vedere al numarului de trofee, Dinamo are in palmares 18 titluri nationale, 13 Cupe ale Romaniei, 2 Supercupe ale Romaniei si o semifinala a Cupei Campionilor Europeni in sezonul 1983_1984.", "Stadionul Dinamo, numit si Stadionul Stefan cel Mare este un stadion din Bucuresti si apartine clubului Dinamo Bucuresti, inaugurat in anul 1952. Stadionul este amplasat in Sectorul 2 din Bucuresti, pe Soseaua Stefan cel Mare."));
-    std::cout << fcdb << std::endl;
+    std::cout <<"\n" << fcdb << "\n";
 
     return 0;
 }
