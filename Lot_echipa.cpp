77448806128e9fd0a509244477b6627b56f7e3c8
LSBogdan <64216638+LSBogdan@users.noreply.github.com>
2021-05-20 10:37:45 +0300

    Update Tema2

--- Lot_echipa.cpp
+++ Lot_echipa.cpp
@@ -1,50 +1,37 @@
-
 #include "Lot_echipa.h"
 
-Lot_echipa::Lot_echipa()
-{
-    nr_jucatori = 0;
-}
+Lot_echipa::Lot_echipa(){}
 
 Lot_echipa::~Lot_echipa()
 {
-    for(int i = nr_jucatori-1; i>=0; i--)
+    for(int i = lot.size()-1; i>=0; i--)
         delete lot[i];
-
-    nr_jucatori = 0;
 }
 
-void Lot_echipa :: add_jucator(Portar jucator)
+void Lot_echipa :: add_jucator(const Portar& jucator)
 {
     lot.push_back(new Portar(jucator));
-    nr_jucatori++;
 }
 
-void Lot_echipa :: add_jucator(Fundas jucator)
+void Lot_echipa :: add_jucator(const Fundas& jucator)
 {
     lot.push_back(new Fundas(jucator));
-    nr_jucatori++;
-
 }
 
-void Lot_echipa :: add_jucator(Mijlocas jucator)
+void Lot_echipa :: add_jucator(const Mijlocas& jucator)
 {
     lot.push_back(new Mijlocas(jucator));
-    nr_jucatori++;
-
 }
 
-void Lot_echipa :: add_jucator(Atacant jucator)
+void Lot_echipa :: add_jucator(const Atacant& jucator)
 {
     lot.push_back(new Atacant(jucator));
-    nr_jucatori++;
-
 }
 
 
 void Lot_echipa ::afisare_lot()
 {
-    for(int i = 0; i < nr_jucatori; i++)
+    for(int i = 0; i < lot.size(); i++)
         std::cout << *lot[i] << "\n\n";
     std::cout<<"\n";
 }
